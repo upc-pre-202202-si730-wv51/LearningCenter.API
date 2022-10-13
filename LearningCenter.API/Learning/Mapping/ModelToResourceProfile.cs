3856eb9f0dc651068589a0f24143e8a5d894da04
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-13 14:14:29 -0500

    chore: added automapper profile classes.

---
+++ LearningCenter.API/Learning/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace LearningCenter.API.Learning.Mapping;
+
+public class ModelToResourceProfile : Profile
+{
+    protected ModelToResourceProfile()
+    {
+        
+    }
+}
---
+++ LearningCenter.API/Learning/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+
+namespace LearningCenter.API.Learning.Mapping;
+
+public class ResourceToModelProfile : Profile
+{
+    protected ResourceToModelProfile()
+    {
+        
+    }
+}
