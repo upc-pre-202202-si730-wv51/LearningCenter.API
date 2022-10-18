8b8e44c563074f30444f91f02097c29e3c6f7f73
Angel Velasquez <pcisavel@upc.edu.pe>
2022-10-18 14:25:27 -0500

    feat(tutorials): specified object-relational mapping rules for tutorials, also added missing description property.

--- LearningCenter.API/Learning/Domain/Models/Tutorial.cs
+++ LearningCenter.API/Learning/Domain/Models/Tutorial.cs
@@ -5,6 +5,8 @@ public class Tutorial
     public int Id { get; set; }
     public string Title { get; set; }
     
+    public string Description { get; set; }
+    
     // Relationships
     public int CategoryId { get; set; }
     public Category Category { get; set; }
