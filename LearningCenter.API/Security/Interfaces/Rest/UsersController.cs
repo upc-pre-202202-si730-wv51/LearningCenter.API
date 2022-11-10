a03b6a6d43428085b4fdc2c9fd7d1a5eb6e8c11c
Angel Velasquez <pcisavel@upc.edu.pe>
2022-11-10 13:43:34 -0500

    feat(security): implemented users controller.

---
+++ LearningCenter.API/Security/Interfaces/Rest/UsersController.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using LearningCenter.API.Security.Authorization.Attributes;
+using LearningCenter.API.Security.Domain.Models;
+using LearningCenter.API.Security.Domain.Services;
+using LearningCenter.API.Security.Domain.Services.Communication;
+using LearningCenter.API.Security.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Security.Interfaces.Rest;
+
+[Authorize]
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("sign-in")]
+    public async Task<ActionResult> Authenticate(AuthenticateRequest request)
+    {
+        var response = await _userService.Authenticate(request);
+        return Ok(response);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("sign-up")]
+    public async Task<IActionResult> Register(RegisterRequest request)
+    {
+        await _userService.RegisterAsync(request);
+        return Ok(new { message = "Registration successful" });
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = await _userService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<User>, IEnumerable<UserResource>>(users);
+        return Ok(resources);
+    }
+        
+}
